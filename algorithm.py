daf0e796c57ba48d5c03a1c720e55c216dc6a99f
utruong309 <uyenthutruong09@gmail.com>
2026-03-20 02:38:23 -0400

    fixed parsing bug

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,7 @@
 #IMPORTS
 import sys
 from collections import OrderedDict
+import time as tm
 
 # NOTES:
 # 1. Need to adjust code for different sized INPUTS
@@ -23,7 +24,7 @@ i = -1 #for construction preference list
 pref_list = []
 popularity = {}
 j = 0 #for constructing teacher conflict
-teacher_conflict = [0] * 15 #[0,10,4,...] says 10 conflicts with 1, 4 conflicts with 2
+teacher_conflict = []  # FIXED (removed hardcode 15)
 class_teacher = [] #['class','teacher]...
 cID_IID = {} #{class:teacher}
 pop = {} #for compute overlap
@@ -54,11 +55,19 @@ with open(pref_file, 'r') as pref_unclean:
 
 with open(sys.argv[2], "r") as contraints_file:
     for line in contraints_file:
-        if j < 8:
-            j +=1
+        parts = line.strip().split()
+        
+        # skip header lines
+        if len(parts) < 2:
             continue
-        class_teacher.append(line.split())
+        if not parts[0].isdigit():
+            continue
+        
+        # only keep class-teacher pairs
+        class_teacher.append(parts)
 
+max_class_id = max(int(c[0]) for c in class_teacher)
+teacher_conflict = [0] * (max_class_id + 1)
 
 #FUNCTIONS
 
@@ -111,26 +120,6 @@ overlap_conflict, popularity = compute_overlap(pref_list)
 # we need overlapping
 # now we need rank class popularity
 
-
-j = 0
-teacher_conflict = [0] * 15
-class_teacher = []
-
-with open(sys.argv[2], "r") as contraints_file:
-    for line in contraints_file:
-        if j < 8:
-            j +=1
-            continue
-        class_teacher.append(line.split())
-
-for a in class_teacher:
-    tchr1 = int(a[1])
-    for b in class_teacher:
-        tchr2 = int(b[1])
-        if tchr1 == tchr2 and a[0] != b[0]:
-            teacher_conflict[int(a[0])] = int(b[0])
-            teacher_conflict[int(b[0])] = int(a[0])
-# avoid overlap class and teacher class
 
 time_slots = {}
 times = 4
@@ -200,7 +189,9 @@ def create_class_objects(room_slots, pref_list, cID_IID):
         times_enrolled = [0,0,0,0]
         for i in range(1,4):
             clssID = int(list[i])
-            class_Class = sorted_objects[clssID-1]
+            class_Class = next((c for c in sorted_objects if c.ID == clssID), None)
+            if class_Class is None:
+                continue
             #for each class on pref list check that student is available
             #at that time, else don't enroll them and count 
             if times_enrolled[class_Class.time - 1] == 0:
@@ -226,19 +217,28 @@ def output_schedule(objects_list):
         output = str(clss.ID) + "\t" + str(clss.room) + "\t" + str(clss.teacherID) + "\t" + str(clss.time) + "\t" + string
         sys.stdout.write(output)
         sys.stdout.write("\n") 
-            
-
 
 #MAIN, FUNCTION CALLS
-overlap_conflict, popularity = compute_overlap(pref_list)
-divide_into_slots(overlap_conflict, teacher_conflict)
-print(time_slots)
-room_slots = divide_into_rooms(popularity)
-print(room_slots)
-    
+start = tm.time()
 
-divide_into_slots(overlap_conflict, teacher_conflict)
-print(overlap_conflict)
-print(teacher_conflict)
-print(time_slots)
-print(popularity)
+overlap_conflict, popularity = compute_overlap(pref_list)
+time_slots = divide_into_slots(overlap_conflict, teacher_conflict)
+room_slots = divide_into_rooms(popularity) 
+
+objects_list = create_class_objects(room_slots, pref_list, cID_IID) 
+actual_value = sum(len(c.students) for c in objects_list)
+best_value = len(pref_list) * 4
+fit = actual_value / best_value 
+
+end = tm.time()
+
+print("Student preference value:", actual_value)
+print("Best case student preference value:", best_value)
+print("Fit percentage:", fit)
+print("Time:", end - start)
+    
+# divide_into_slots(overlap_conflict, teacher_conflict)
+# print(overlap_conflict)
+# print(teacher_conflict)
+# print(time_slots)
+# print(popularity)
